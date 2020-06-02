e416fef131d0b5544d84c661e43687da71b35bb9
liangcongcongcc <tt_liangcongcong@163.com>
2020-06-02 17:23:43 +0800

    这是第二次提交

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
-	public static void main(String [] ages){
+	public static void main(String[]args){
 
-​			System.out.println("梁聪聪！！！");
+​		System.out.println("梁聪聪！！！");
+		System.out.println("梁聪聪2");
 
 }
 }
