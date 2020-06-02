a9ecb4859453abe0d93150dbe1e4f079ae47a842
yangpengyun <66256667+yangpengyun@users.noreply.github.com>
2020-06-02 20:56:38 +0800

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,11 +1,3 @@
-public class Hello{
-	public static void main(String[]args){
-
-​		System.out.println("梁聪聪！！！");
-		System.out.println("梁聪聪2");
-		System.out.println("梁聪聪5");
-		System.out.println("梁聪聪6");
-		System.out.println("8");
-
-}
+public class test{
+	
 }
